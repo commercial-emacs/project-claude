1d617eecdbb5b70e4c4ce1beaeb642a418fd8def
dickmao <none>
2026-04-14 17:58:05 -0400

    test insert-file-ref

--- test-template.el
+++ test-template.el
@@ -8,7 +8,30 @@
 	 (project-@PROVIDER@/invocation (format "echo foo && sleep 20"))
 	 (buf (project-@PROVIDER@ :no-solicit t)))
     (should (eq buf (current-buffer)))
-    (should (project-@PROVIDER@//wait-for (regexp-quote "foo")))))
+    (should (project-@PROVIDER@//wait-for (regexp-quote "foo")))
+    (let (kill-buffer-query-functions)
+      (kill-buffer buf))))
+
+(ert-deftest @PROVIDER@-current-prefix-arg-insert-file-ref ()
+  "File reference is relative path to emacs-ghostty subdir."
+  (cl-letf* ((ghostty-vt-mode-hook) ;neutralize project-gemini/clear-on-startup
+	     ((symbol-function 'project-remember-project) #'identity)
+	     (project-@PROVIDER@/invocation "/bin/sh")
+	     (project-@PROVIDER@/prompt-regex "$")
+	     (parent-dir default-directory)
+	     (parent-project (project-current))
+	     (default-directory (expand-file-name "emacs-ghostty"))
+	     (current-project (project-current)))
+    (should-not (equal parent-project current-project))
+    (project-@PROVIDER@/ensure-ready
+     (should (equal (project-current) current-project)))
+    (let ((default-directory parent-dir))
+      (project-root-find-file "test-template.el")
+      (should (equal parent-project (project-current)))
+      (let ((current-prefix-arg '(4)))
+	(call-interactively #'project-@PROVIDER@/insert-file-ref)
+	(goto-char (point-min))
+	(should (re-search-forward (regexp-quote "@../test-template.el:1") nil t))))))
 
 (provide 'test-project-@PROVIDER@-generated)
 ;;; test-project-@PROVIDER@-generated.el ends here
